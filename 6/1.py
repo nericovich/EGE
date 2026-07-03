2c642f9b8efca3541e6df793e8b584265843b2a6
Neri <akazeevae@gmail.com>
2026-07-04 01:58:21 +0300

    q

--- 6/1.py
+++ 6/1.py
@@ -1,16 +1,17 @@
 from turtle import *
 tracer(0)
+ #speed 10
 k = 15
 
 screensize(3500, 2500)
 right(315)
 for _ in range(7):
-    fd(72 *k)
-    rt(45)
+    fd(72 *k) # forward(x)
+    rt(45) #right(x)
     fd(43*k)
     rt(135)
 
-up()
+up() # down()
 
 for x in range(-30, 100):
     for y in range(-30, 100):
